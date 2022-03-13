aaa9059305360ff38b33018830fac748af800009
Mohamed Reda Legzali <m.reda.legzali@gmail.com>
2022-03-13 13:18:12 +0100

    13-03-2022

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,9 @@
 Import-Module oh-my-posh
 Import-Module PSReadLine
+
 Set-PoshPrompt -Theme amro
+
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+Set-Alias -Name bs -Value busybox -Description "Busybox utility"
