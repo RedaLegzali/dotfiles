0a8e09ccebcd55fb23b1c68c1b491513624b8cc1
RedaLegzali <49563755+RedaLegzali@users.noreply.github.com>
2022-06-03 01:51:14 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,17 +1,38 @@
-set -gx EDITOR vim
-set -gx TERM xterm-256color
-set -gx FZF_DEFAULT_COMMAND fdfind
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	set -gx EDITOR vim
+	set -gx TERM xterm-256color
+	set -gx FZF_DEFAULT_COMMAND fd
+	set -gx CHROME_EXECUTABLE chromium
+	
+	fish_add_path ~/.local/bin
+	fish_add_path ~/.gem/ruby/3.0.0/bin/
+	fish_add_path ~/.config/composer/vendor/bin
+	fish_add_path ~/Android/Sdk/cmdline-tools/latest/bin
+	fish_add_path ~/Android/Sdk/emulator
+	fish_add_path ~/Android/Sdk/platform-tools
+	fish_add_path /opt/sonar-scanner/bin/
 
-alias sudo "sudo -E"
+	alias sudo "sudo -E"
 
-function clean-pipenv
-	rm -rf ~/.local/share/virtualenvs/*
+	function clean-pipenv
+		rm -rf ~/.local/share/virtualenvs/*
+	end
+
+	function clean-kube
+		rm -rf ~/.kube/*
+	end
+
+	function clean-docker
+		docker container prune -f
+		docker image prune -f
+		docker network prune -f
+		docker volume prune -f
+	end
+
+	starship init fish | source
+	kubectl completion fish | source
+	pip completion --fish | source
+
+	screenfetch
 end
-function clean-docker
-	docker container prune -f
-	docker image prune -f
-	docker network prune -f
-	docker volume prune -f
-end
-
-starship init fish | source
