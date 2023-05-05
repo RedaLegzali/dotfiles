3f205dd6a54169dea442f2b252f368c57f0bfa19
RedaLegzali <49563755+RedaLegzali@users.noreply.github.com>
2023-05-05 15:21:05 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,16 @@
-Import-Module oh-my-posh
+oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/star.omp.json" | Invoke-Expression
+
 Import-Module PSReadLine
 
-Set-PoshPrompt -Theme amro
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 Set-Alias -Name bs -Value busybox -Description "Busybox utility"
