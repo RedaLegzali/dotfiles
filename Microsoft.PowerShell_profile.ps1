d227299fb3d4f8b1c2fcad88d34b425443289c4f
RedaLegzali <49563755+RedaLegzali@users.noreply.github.com>
2024-05-15 14:41:50 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,8 @@ oh-my-posh init pwsh | Invoke-Expression
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/star.omp.json" | Invoke-Expression
 
 Import-Module PSReadLine
+Import-Module DockerCompletion
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 
 # Shows navigable menu of all options when hitting Tab
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
@@ -13,13 +15,6 @@ Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-Set-Alias -Name bs -Value busybox -Description "Busybox utility"
-
 # rbenv for Windows
 $env:RBENV_ROOT = "C:\Ruby-on-Windows"
-
-# Not easy to download on Github?
-# Use a custom mirror!
-# $env:RBENV_USE_MIRROR = "https://abc.com/abc-<version>"
-
 & "$env:RBENV_ROOT\rbenv\bin\rbenv.ps1" init
